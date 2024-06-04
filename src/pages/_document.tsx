fe6958d7764a785f638331e230945f8b1067553f
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-06-04 12:52:43 -0300

    Meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,13 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
 	return (
 		<Html lang="pt-br">
-			<Head />
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta name="author" content="Matheus Zanela Picoli" />
+				<meta name="description" content="Um chatbot bem simples construído em Next JS com Typescript e Tailwind CSS." />
+				<title>MP Chatbot</title>
+			</Head>
+
 			<body>
 				<Main />
 				<NextScript />
