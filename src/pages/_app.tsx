4b76dd0bf2632bd273f120b0ec9ebb41618dc7f2
Matheus Picoli <matheuspicoli2011@gmail.com>
2024-06-05 09:58:38 -0300

    next _app with arrow function

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,9 @@
 //- CSS
-import "@/styles/globals.css";
+import "@/styles/globals.css"
 
 //- Next
 import type { AppProps } from "next/app"
 
-export default function App<T extends AppProps>(Next: T) {
-	return <Next.Component {...Next.pageProps} />
-}
+const App = <T extends AppProps>(Next: T) => <Next.Component {...Next.pageProps} />
+
+export default App
